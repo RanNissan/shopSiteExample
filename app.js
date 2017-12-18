02b1c841b43a5a51687f254ffafcfac2b1629898
Ran Nissan <ranissan8@gmail.com>
2017-12-18 13:49:56 +0200

    feat(MainPage) = basic Main page is finished.

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var app = express();
 
 // view engine setup
 app.use(express.static(__dirname + '/public/Views'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
